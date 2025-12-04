3cbcbd3bc9090053d09227d8088d0d972bb407ce
Collins Chikangwu <collinschristroa@gmail.com>
2025-12-04 23:33:16 +0100

    feat: add logging for session cookies in proxy function
    - Implemented console logging for session cookies in the proxy function to aid in debugging and monitoring authentication flow.

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 
 export async function proxy(request: NextRequest) {
   const cookies = getSessionCookie(request);
+  console.log('cookies', cookies);
   if (!cookies) {
     return NextResponse.redirect(new URL('/register', request.url));
   }
